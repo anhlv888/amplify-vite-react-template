342772c91d5682fd59276e119a2b22fc856c9cef
vietanhhnl <viet.anh@consultinghnl.vn>
2025-08-01 00:47:15 +0700

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -40,6 +40,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <h1>{user?.signInDetails?.loginId}</h1>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
